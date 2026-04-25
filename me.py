a5c156ce5eb3308580cc57a5a508906c8741b220
Bathul sultana <bathulsultana@gmail.com>
2026-04-25 09:23:22 +0530

    added a program

---
+++ me.py
@@ -0,0 +1,10 @@
+n = int(input("my name is:" ))
+if (n<=100):
+    print("my name is bathul")
+elif n>100:
+    print("my name is sultana")
+elif(n==0):
+    print("katlagal")
+else:
+    print("not a human")
+    
